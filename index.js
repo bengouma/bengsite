3981ef436b8148bc02a2d7d81ce5d51db935d257
Ben Goumalatsos <bengoumalatsos@hotmail.com>
2020-06-17 01:28:04 -0400

    Edit javascript formatting

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 const server = app.listen(8080, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
+  console.log(`Express running → PORT ${server.address().port}`);
+});
 
 app.get('/', (req,res) => {
-    res.render('main');
+  res.render('main');
 });
